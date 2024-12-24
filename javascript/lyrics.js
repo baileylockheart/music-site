22974f4cc86d5e4b257c712d8a06471dcbf52eff
Bailey / Jay ⋆˚🐾˖° <150482050+baileylockheart@users.noreply.github.com>
2024-12-23 21:11:21 -0800

    Add files via upload

---
+++ javascript/lyrics.js
@@ -0,0 +1,48 @@
+function myMind() {
+    var x = document.getElementById("mymind");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
+      function bigHug() {
+    var x = document.getElementById("bighug");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
+      function existentialCrisis() {
+    var x = document.getElementById("existentialcrisis");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
+      function realToMe() {
+    var x = document.getElementById("realtome");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
+      function thought() {
+    var x = document.getElementById("thought");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
+      function beyondTheHorizon() {
+    var x = document.getElementById("beyondthehorizon");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
