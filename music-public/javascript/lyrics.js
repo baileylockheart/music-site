6d33350c57447116a7655a7353e818cd31ea91c5
Bailey Lockheart <baileylockheart@gmail.com>
2025-01-15 20:39:46 -0800

    made lyric dropdowns easier to use!!

--- music-public/javascript/lyrics.js
+++ music-public/javascript/lyrics.js
@@ -1,48 +1,18 @@
-      function myMind() {
-    var x = document.getElementById("mymind");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+  function myMind() {
+    document.getElementById("mymind").classList.toggle("show");
+  }
+  function bigHug() {
+    document.getElementById("bighug").classList.toggle("show");
+  }
+  function existentialCrisis() {
+    document.getElementById("existentialcrisis").classList.toggle("show");
   } 
-      function bigHug() {
-    var x = document.getElementById("bighug");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+  function realToMe() {
+    document.getElementById("realtome").classList.toggle("show");
   } 
-      function existentialCrisis() {
-    var x = document.getElementById("existentialcrisis");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+  function Thought() {
+    document.getElementById("thought").classList.toggle("show");
   } 
-      function realToMe() {
-    var x = document.getElementById("realtome");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  } 
-      function thought() {
-    var x = document.getElementById("thought");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  } 
-      function beyondTheHorizon() {
-    var x = document.getElementById("beyondthehorizon");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  } 
+  function beyondTheHorizon() {
+    document.getElementById("beyondthehorizon").classList.toggle("show");
+  }
