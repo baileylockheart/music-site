a4820dd5f66d925ca75a970f32a623d27f53799b
Bailey <baileylockheart@gmail.com>
2025-03-28 00:21:50 -0700

    VERSION TWOOOOOO

---
+++ public/v1/javascript/lyrics.js
@@ -0,0 +1,18 @@
+  function myMind() {
+    document.getElementById("mymind").classList.toggle("show");
+  }
+  function bigHug() {
+    document.getElementById("bighug").classList.toggle("show");
+  }
+  function existentialCrisis() {
+    document.getElementById("existentialcrisis").classList.toggle("show");
+  } 
+  function realToMe() {
+    document.getElementById("realtome").classList.toggle("show");
+  } 
+  function Thought() {
+    document.getElementById("thought").classList.toggle("show");
+  } 
+  function beyondTheHorizon() {
+    document.getElementById("beyondthehorizon").classList.toggle("show");
+  }
